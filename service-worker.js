dc71af11c0dad13f7047b0415d871705e1c1737d
qmsmumbai-technician <qmsmumbai@gmail.com>
2026-08-26 14:47:17 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "kaarigar-merged-v1";
+const CACHE = "kaarigar-merged-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./splash.jpg"];
 
 self.addEventListener("install", (e) => {
